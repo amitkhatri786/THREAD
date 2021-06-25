566e485622722c3be6a9b2842b4dc3c5b10fbced
Amit Khatri <amit7861234@gmail.com>
2021-06-25 21:33:19 +0530

    producer consumer v2

--- PRODUCER_CONSUMER/producer_consumer.c
+++ PRODUCER_CONSUMER/producer_consumer.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<semaphore.h>
 #include<pthread.h>
-
+#include<stdlib.h>
 
 sem_t empty;
 sem_t full;
 pthread_mutex_t mylock;
 
 int g_buffer[10];
+int p_count =0;
+int c_count = 0;
 int count =0;
 
 void *producer(void *arg)
@@ -17,12 +19,17 @@ void *producer(void *arg)
 	while(1) {
 		sem_wait(&empty);
 		pthread_mutex_lock(&mylock);
+		g_buffer[p_count] = random();
+		printf("[%s - %d] p_count = %d g_buffer[%d] = %d\n",__func__,__LINE__,p_count,p_count,g_buffer[p_count]);
+		//p_count = (p_count+1)%10;
+		p_count = p_count+1;
+		pthread_mutex_unlock(&mylock);
+		sem_post(&full);
+	}
 		g_buffer[count] = count ;
 		count++;
 		pthread_mutex_unlock(&mylock);
 		sem_post(&full);
-	}
-
 }
 
 void *consumer(void *arg)
@@ -34,13 +41,18 @@ void *consumer(void *arg)
 	while(1) {
 		sem_wait(&full);
 		pthread_mutex_lock(&mylock);
+		//t = g_buffer[c_count];
+		t = g_buffer[--p_count];
+		printf("[%s - %d] p_count = %d t = %d\n",__func__,__LINE__,p_count,t);
+	//	c_count = (c_count +1)%10;
+		pthread_mutex_unlock(&mylock);
+		sem_post(&empty);
+	}
 		t = g_buffer[count];
 		count --;
 		printf("t = %d\n",t);
 		pthread_mutex_unlock(&mylock);
 		sem_post(&empty);
-	}
-
 }
 
 int main()
@@ -50,6 +62,11 @@ int main()
 	sem_init(&empty,0,10);
 	sem_init(&full,0,0);
 	pthread_create(&t1,NULL,producer,"Producer");
+	pthread_create(&t2,NULL,consumer,"Consumer");
+
+	printf("You are in main\n");
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
 	pthread_create(&t1,NULL,consumer,"Consumer");
 
 	printf("You are in main\n");
